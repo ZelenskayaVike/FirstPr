2ef7f7dcec3d1ce06135321b17d29ca540085950
ZelenskayaVike <40124852+ZelenskayaVike@users.noreply.github.com>
2021-05-03 16:47:10 +0300

    Update table.js

--- src/table.js
+++ src/table.js
@@ -1,4 +1,11 @@
-class Fech(){
+class User {
+    constructor(name, email, tel, info) {
+        this.name = name;
+        this.email=email;
+        this.tel=tel;
+        this.info=info;
+    }
 
-    con
 }
+const  eris=new User("eric","er@com.ua","+3807673034", "I like books");
+console.log(eris);
